aa558f085aa559a4d87dc674dac1ec032d782c6b
rvnsource <ravindran.shan@gmail.com>
2024-08-19 21:17:50 +0530

    Initial gcp ai project files/directories with demo_gemini and data/json/serviceaccount.json file

---
+++ src/demo_gemini.py
@@ -0,0 +1,16 @@
+# Initialize Vertex AI
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
+vertexai.init(project="river-span-431711-k8", location="us-central1")
+
+# Load the Gemini 1.5 Pro model. (https://cloud.google.com/vertex-ai/docs/reference/python/latest/vertexai.generative_models)
+multimodal_model = GenerativeModel("gemini-1.5-pro-001")
+
+contents = ["Explain LLM"]
+
+response = multimodal_model.generate_content(contents)
+
+print(response.text)
+
+
