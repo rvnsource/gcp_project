4f7eda7cd2da3376d8d59d94cde5d1fc2d17fd7a
rvnsource <ravindran.shan@gmail.com>
2024-08-22 14:46:30 +0530

    Clean up the code a bit to work. Tested to be working.

--- src/demo_gemini.py
+++ src/demo_gemini.py
@@ -1,12 +1,8 @@
-# Test changes - 1
-# Initialize Vertex AI
 
 import vertexai
 from vertexai.generative_models import GenerativeModel
 
-vertexai.init(project="river-span-431711-k8", location="us-central1")
-
-# Load the Gemini 1.5 Pro model. (https://cloud.google.com/vertex-ai/docs/reference/python/latest/vertexai.generative_models)
+vertexai.init(project="river-span-431711-k8", location="us-central1")# Load the Gemini 1.5 Pro model. (https://cloud.google.com/vertex-ai/docs/reference/python/latest/vertexai.generative_models)
 multimodal_model = GenerativeModel("gemini-1.5-pro-001")
 
 contents = ["Explain LLM"]
