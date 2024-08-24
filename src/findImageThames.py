0d829c76cb926ae9a58c35dac0b2f53f156dd290
rvnsource <ravindran.shan@gmail.com>
2024-08-25 00:01:15 +0530

    improved different prompt

--- src/findImageThames.py
+++ src/findImageThames.py
@@ -9,8 +9,9 @@ vertexai.init(project=PROJECT_ID, location=REGION)
 #IMAGE_FILE = "gs://generativeai-downloads/images/scones.jpg"
 IMAGE_FILE = "/Users/ravi/gcp_project/data/raw/By_The_River_Thames_at_Vauxhall,_London_-_geograph.org.uk_-_5726285.jpg"
 image = Image.load_from_file(IMAGE_FILE)
+prompt = "What is shown in this image? and give 3 more similar places in the world"
 
 generative_multimodal_model = GenerativeModel("gemini-1.5-flash-001")
-response = generative_multimodal_model.generate_content(["What is shown in this image?", image])
+response = generative_multimodal_model.generate_content([prompt, image])
 
 print(response.text)
